b32b8c8c27f659f493f94e82fed5cba89c274636
Aaron Contreras <aaronlcaq@gmail.com>
2021-02-09 11:57:16 -0500

    Add ability to comment on post from feed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,21 @@ class CommentsController < ApplicationController
   end
 
   def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+
+    if post.save
+      flash[:notice] = 'Your comment has been added to the post.'
+    else
+      flash[:alert] = 'Fix up a couple things before commenting.'
+    end
+
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
