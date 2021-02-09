0bfb3dbf3dde457886ccee6eedd4040871561e6d
Aaron Contreras <aaronlcaq@gmail.com>
2021-02-09 10:45:54 -0500

    Create comments controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :new, :create, :destroy] do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:new, :create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
