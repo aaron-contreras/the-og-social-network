92cd57e447c596cecaf2dede73fc62ee21b01d42
Aaron Contreras <aaronlcaq@gmail.com>
2021-02-18 11:41:48 -0500

    Add specs for session creation

--- spec/system/log_ins_spec.rb
+++ spec/system/log_ins_spec.rb
@@ -5,5 +5,41 @@ RSpec.describe "LogIns", type: :system do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context "when user's credentials are correct"  do
+    it 'logs in the user' do
+      user = FactoryBot.create(:user)
+      visit new_user_session_path
+      fill_in('Email', with: user.email)
+      fill_in('Password', with: user.password)
+      click_button 'Log in'
+      expect(page).to have_current_path(root_path)
+      expect(page).to have_content('successfully')
+    end
+  end
+
+  context "when credentials are incorrect"  do
+    context 'when email is wrong' do
+      it "doesn't log the user in" do
+        user = FactoryBot.create(:user)
+        visit new_user_session_path
+        fill_in('Email', with: "#{user.email}1")
+        fill_in('Password', with: user.password)
+        click_button 'Log in'
+        expect(page).to have_current_path(new_user_session_path)
+        expect(page).to have_content('Invalid')
+      end
+    end
+
+    context 'when password is wrong' do
+      it "doesn't log the user in" do
+        user = FactoryBot.create(:user)
+        visit new_user_session_path
+        fill_in('Email', with: user.email)
+        fill_in('Password', with: "#{user.password}1")
+        click_button 'Log in'
+        expect(page).to have_current_path(new_user_session_path)
+        expect(page).to have_content('Invalid')
+      end
+    end
+  end
 end
